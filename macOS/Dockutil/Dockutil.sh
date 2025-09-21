e438acdd4990d27c48f4807231e7190deda1fc05
Dominic Brant <dombrant@gmail.com>
2025-09-20 20:33:06 -0600

    updated Dockutil.sh to set the Launchpad path based on the macOS version

--- macOS/Dockutil/Dockutil.sh
+++ macOS/Dockutil/Dockutil.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 
+# ==================================================
+# Dockutil
+# Sets the dock on Macs
+# Contributor(s):
+#    - Dominic Brant
+# Version 1.1 - 09/17/2025
 # Original script taken from: https://community.jumpcloud.com/t5/community-scripts/using-dockutil-to-set-a-user-s-dock/m-p/1897
+# ==================================================
 
 # Check if dockutil is installed, if not throw an error
 if [ ! command -v dockutil &>/dev/null ];
@@ -14,6 +21,13 @@ Killall="/usr/bin/killall"
 LoggedInUser=$(ls -l /dev/console | awk '{print $3}')
 LoggedInUserHome="/Users/$LoggedInUser"
 UserPlist=$LoggedInUserHome/Library/Preferences/com.apple.dock.plist
+LaunchpadPath="/System/Applications/Launchpad.app"
+
+# Check if the Mac is running Tahoe (26.0) or later, and if so change Launchpad.app to Apps.app
+if [[ "$(sw_vers -productVersion)" -ge 26.0 ]];
+then
+    LaunchpadPath="/System/Applications/Apps.app"
+fi
 
 ################################################################################
 # Use Dockutil to Modify Logged-In User's Dock
@@ -21,7 +35,7 @@ UserPlist=$LoggedInUserHome/Library/Preferences/com.apple.dock.plist
 echo "Removing all items from the dock for the user $LoggedInUser..."
 sudo -u $LoggedInUser dockutil --remove all --no-restart $UserPlist
 echo "Creating new dock..."
-sudo -u $LoggedInUser dockutil --add "/System/Applications/Launchpad.app" --no-restart $UserPlist
+sudo -u $LoggedInUser dockutil --add $LaunchpadPath --no-restart $UserPlist
 sudo -u $LoggedInUser dockutil --add "/System/Cryptexes/App/System/Applications/Safari.app" --no-restart $UserPlist
 sudo -u $LoggedInUser dockutil --add "/Applications/Self Service.app" --no-restart $UserPlist
 sudo -u $LoggedInUser dockutil --add "/Applications/Slack.app" --no-restart $UserPlist
